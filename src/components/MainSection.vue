8a2e967160ab45b491e44bf472d4ba93c4947f07
TapeMate <admin@tapemate.onmicrosoft.com>
2024-08-11 14:44:50 +0200

    styling changes for main section.

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,21 +1,19 @@
 <template>
   <section class="section-main-container">
-    <div class="section-main-background">
+    <div class="section-main-banner-container">
       <h3 class="section-main-header">dummy header main</h3>
-      <div class="section-main-banner-container">
-        <div class="section-main-logo"></div>
-        <div class="section-main-content">
-          <div
-            v-for="(item, index) in bannerContent"
-            :key="index"
-            class="banner-content"
-          >
-            {{ item.content }}
-          </div>
+      <div class="section-main-logo"></div>
+      <div class="section-main-content">
+        <div
+          v-for="(item, index) in bannerContent"
+          :key="index"
+          class="banner-content"
+        >
+          {{ item.content }}
         </div>
-        <div class="section-main-options">
-          <button class="main-option-btn">OPTION BUTTON</button>
-        </div>
+      </div>
+      <div class="section-main-options">
+        <button class="main-option-btn">OPTION BUTTON</button>
       </div>
     </div>
   </section>
