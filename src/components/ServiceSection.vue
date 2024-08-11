f20db70146824b1f7c214fd5866093af39eb940a
TapeMate <admin@tapemate.onmicrosoft.com>
2024-08-11 14:58:12 +0200

    created basic layout for service section component.

--- src/components/ServiceSection.vue
+++ src/components/ServiceSection.vue
@@ -1,23 +1,21 @@
 <template>
   <section class="section-service-container">
-    <div class="section-service-background">
-      <h3 class="section-service-header">dummy header service</h3>
-      <div class="section-service-items">
-        <div
-          v-for="(item, index) in serviceContent"
-          :key="index"
-          class="service-item"
-        >
-          <h4 class="service-item-header">{{ item.header }}</h4>
-          <p class="service-item-description">{{ item.description }}</p>
-        </div>
+    <h3 class="section-service-header">dummy header service</h3>
+    <div class="section-service-items">
+      <div
+        v-for="(item, index) in serviceContent"
+        :key="index"
+        class="service-item"
+      >
+        <h4 class="service-item-header">{{ item.header }}</h4>
+        <p class="service-item-description">{{ item.description }}</p>
+      </div>
 
-        <!-- 
+      <!-- 
         potential for own components.
         consider tiny animations.
         telling a story for each service example.
         -->
-      </div>
     </div>
   </section>
 </template>
