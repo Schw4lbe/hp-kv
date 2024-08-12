9b454984a5fba32c5e48930d088f801b6cb7928c
TapeMate <admin@tapemate.onmicrosoft.com>
2024-08-12 22:08:22 +0200

    added all needed controll icons to modal component. created basic styling.

--- src/components/ImgModal.vue
+++ src/components/ImgModal.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="modal-container">
-    <p>{{ title }}</p>
-    <p>{{ description }}</p>
+    <!-- modal control elements: -->
+    <button @click="$emit('closeModal')" class="close-modal-btn">
+      <img class="modal-close-icon" :src="close" alt="Schließen Symbol" />
+    </button>
+    <button class="next-image">
+      <img :src="next" alt="Nächstes Bild Symbol" class="next-icon" />
+    </button>
+    <button class="prev-image">
+      <img :src="prev" alt="Vorheriges Bild Symbol" class="pref-icon" />
+    </button>
+
+    <h4 class="modal-header">{{ title }}</h4>
     <img :src="image" alt="Galeriebild" class="modal-gallery-image" />
-    <button @click="$emit('closeModal')" class="close-modal-btn">close</button>
+    <p class="modal-description">{{ description }}</p>
   </div>
 </template>
 
 <script>
+import closeIcon from "../../public/img/icons/close.svg";
+import nextImgIcon from "../../public/img/icons/arrow-next.svg";
+import prevImgIcon from "../../public/img/icons/arrow-prev.svg";
+
 export default {
   name: "ImgModal",
+
+  data() {
+    return {
+      close: closeIcon,
+      next: nextImgIcon,
+      prev: prevImgIcon,
+    };
+  },
 
   props: {
     title: String,
@@ -18,3 +40,5 @@ export default {
   },
 };
 </script>
+
+<style scoped></style>
